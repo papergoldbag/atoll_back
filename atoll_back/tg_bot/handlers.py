9430780443af961e8a022c466daf59c1d64519c2
Ivan122727 <ermolovivan2018@gmail.com>
2023-04-28 03:08:58 +0500

    events

--- atoll_back/tg_bot/handlers.py
+++ atoll_back/tg_bot/handlers.py
@@ -8,8 +8,18 @@ from atoll_back.core import dp, db
 from atoll_back.models import Event
 from atoll_back.services import get_events
 
+import emoji
+
 log = logging.getLogger(__name__)
 
+async def get_event_description(event: Event):
+    text = f":timer_clock: <strong>Дата</strong>: {str(event.start_dt).split(' ')[0]}\n:party_popper: <strong>Название</strong>: {event.description} \n"
+    if len(event.team_oids) == 0:
+        text += f":Statue_of_Liberty: Пока команды не записались\n"
+    else:
+        for team in event.team_oids:
+            text +=  f":people_holding_hands: {team}\n"
+    return text
 
 @dp.message_handler(commands=TgBotCommands.start)
 async def on_cmd_start(message: types.Message):
@@ -23,9 +33,7 @@ async def on_cmd_events(message: types.Message):
 
     text = ":bookmark_tabs: Мероприятия:\n"
     for event in events:
-        # TODO
-        pass
-
+        text += await get_event_description(event)
     await message.answer(text=emojize(text))
 
 
