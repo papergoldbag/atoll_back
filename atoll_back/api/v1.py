beb53950cd1d956b6c02f118407b1d1cef3a17de
papergoldbag <sabarsenrash@gmail.com>
2023-04-28 06:34:35 +0300

    _

--- atoll_back/api/v1.py
+++ atoll_back/api/v1.py
@@ -685,7 +685,7 @@ async def accept_event_request(
     event = await event_request_to_event(event_request_oid=ev_req.oid)
     await send_from_tg_bot(
         text=(
-            f"<b>Появилось новое мероприятие {event.title}\n"
+            f"<b>Появилось новое мероприятие {event.title}\n</b>"
             f"<a href='https://atoll.divarteam.ru/events/{event.int_id}'>Подробнее</a>'"
         ),
         to_roles=UserRoles.set()
