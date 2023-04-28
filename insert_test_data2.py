5f6a19d8c650bbabc981b35cdc6ce0139e089c5b
ReRosu <recrea.tor@yandex.ru>
2023-04-28 04:39:00 +0300

    ins td

--- insert_test_data2.py
+++ insert_test_data2.py
@@ -71,40 +71,55 @@ async def insert_test_data():
         'description': ""
     })
 
-    
-    await db.user_collection.insert_document({
-        'fullname': "Sportsman1",
-        'mail': "",
-        'tokens': [],
-        'birth_dt': None,
-        'tg_username': "",
-        'tg_id': "",
-        'vk_id': "",
-        'roles': [UserRoles.sportsman],
-        'description': ""
-    })
-    await db.user_collection.insert_document({
-        'fullname': "Sportsman2",
-        'mail': "",
-        'tokens': [],
-        'birth_dt': None,
-        'tg_username': "",
-        'tg_id': "",
-        'vk_id': "",
-        'roles': [UserRoles.sportsman],
-        'description': ""
-    })
-    await db.user_collection.insert_document({
-        'fullname': "Sportsman3",
-        'mail': "",
-        'tokens': [],
-        'birth_dt': None,
-        'tg_username': "",
-        'tg_id': "",
-        'vk_id': "",
-        'roles': [UserRoles.sportsman],
-        'description': ""
-    })
+
+    for i in range(5):
+        tu1 = await db.user_collection.insert_document({
+            'fullname': "Sportsman1_{i}",
+            'mail': f"tu1_{i}@test.ru",
+            'tokens': [],
+            'birth_dt': None,
+            'tg_username': "",
+            'tg_id': "",
+            'vk_id': "",
+            'roles': [UserRoles.sportsman],
+            'description': ""
+        })
+        tu2 = await db.user_collection.insert_document({
+            'fullname': "Sportsman2_{i}",
+            'mail': f"tu2_{i}@test.ru",
+            'tokens': [],
+            'birth_dt': None,
+            'tg_username': "",
+            'tg_id': "",
+            'vk_id': "",
+            'roles': [UserRoles.sportsman],
+            'description': ""
+        })
+        tu3 = await db.user_collection.insert_document({
+            'fullname': "Sportsman3_{i}",
+            'mail': f"tu3_{i}@test.ru",
+            'tokens': [],
+            'birth_dt': None,
+            'tg_username': "",
+            'tg_id': "",
+            'vk_id': "",
+            'roles': [UserRoles.sportsman],
+            'description': ""
+        })
+        tteam = await db.team_collection.insert_document({
+            'captain_oid': tu1['oid'],
+            'title': f'test team {i}',
+            'description': f"team with {tu1['fullname']} {tu2['fullname']} {tu3['fullname']}",
+            'user_oids': [tu1['oid'],tu2['oid'],tu3['oid'],]
+        })
+        tevent = await db.event_collection.insert_document({
+            'title': f'event_{i}',
+            'description': 'event {i}',
+            'team_oids': [tteam['oid']],
+            'start_dt': datetime.now(),
+            'end_dt': datetime.now() + timedelta(days=1),
+            'timeline': [Timeline(dt=datetime.now(),text="старт"), Timeline(dt=datetime.now() + timedelta(days=1), text="конец")],
+        })
 
 
     await db.user_collection.insert_document({
@@ -145,12 +160,27 @@ async def insert_test_data():
 
 
     await db.event_collection.insert_document({
-    'title': '',
-    'description': '',
+    'title': 'Всероссийский хакатон фсп 2023 Воронеж',
+    'description': 'Всероссийский хакатон фсп для студентов',
     'team_oids': [],
     'start_dt': datetime.now(),
-    'end_dt': datetime.now() + timedelta(days=1),
-    'timeline': [Timeline(dt=datetime.now(),text="старт")],
+    'end_dt': datetime.now() + timedelta(days=2),
+    'timeline': [
+        Timeline(dt=datetime.now(),text="старт"), 
+        Timeline(dt=datetime.now() + timedelta(days=1), text="чекпоинт"), 
+        Timeline(dt=datetime.now() + timedelta(days=2), text="конец")],
+    })
+    await db.event_collection.insert_document({
+    'title': 'Тестовый хакатон 2023',
+    'description': 'Тестовый хакатон для теста',
+    'team_oids': [],
+    'start_dt': datetime.now(),
+    'end_dt': datetime.now() + timedelta(days=3),
+    'timeline': [
+        Timeline(dt=datetime.now(),text="старт"), 
+        Timeline(dt=datetime.now() + timedelta(days=1), text="чекпоинт"),
+        Timeline(dt=datetime.now() + timedelta(days=2), text="чекпоинт"), 
+        Timeline(dt=datetime.now() + timedelta(days=3), text="конец"), ],
     })
 
 
